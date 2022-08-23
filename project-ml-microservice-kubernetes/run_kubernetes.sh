78544bc0e8fdc1e193468fc67c2a2c338ad3faf4
Ubuntu <ubuntu@ip-172-31-80-215.ec2.internal>
2022-08-23 12:36:00 +0000

    feature: Added CircleCi Integration

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,16 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=maazaxxs/devops1234
+dockerpath='maaszaxxs/devops1234'
 
-# Step 2
+# Step 2:
 # Run the Docker Hub container with kubernetes
-kubectl run predict --image=$dockerpath --port=80
+kubectl run predictor --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
 kubectl get pods
+sleep 30
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward predict 8000:80
+kubectl port-forward predictor 8000:80
